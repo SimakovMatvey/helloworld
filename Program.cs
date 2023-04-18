cbac02d102ecab6839de42ae213dd730875a641e
Simakov <nogla99@inbox.ru>
2023-04-18 23:20:33 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Matvey!");
+            Console.ReadKey();
+        }
+    }
+}
